04e125bf96c0a929ac7143ce8c219cfaf58e64ce
Marshall Perrin <mperrin@stsci.edu>
2013-09-19 18:32:05 -0400

    improved packaging and version tracking

--- setup.py
+++ setup.py
@@ -1,85 +1,147 @@
 #!/usr/bin/env python
-
-import sys
-import imp
-try:
-    # This incantation forces distribute to be used (over setuptools) if it is
-    # available on the path; otherwise distribute will be downloaded.
-    import pkg_resources
-    distribute = pkg_resources.get_distribution('distribute')
-    if pkg_resources.get_distribution('setuptools') != distribute:
-        sys.path.insert(1, distribute.location)
-        distribute.activate()
-        imp.reload(pkg_resources)
-except:  # There are several types of exceptions that can occur here
-    from distribute_setup import use_setuptools
-    use_setuptools()
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# --based on setup.py from astropy--
 
 import glob
 import os
-from setuptools import setup
+import sys
 
+import setuptools_bootstrap
+from setuptools import setup, find_packages
 
+import astropy
+from astropy.setup_helpers import (register_commands, adjust_compiler,
+                                   filter_packages, update_package_files,
+                                   get_debug_option)
+from astropy.version_helpers import get_git_devstr, generate_version_py
 
-setupargs = {
-    'name'          :       'poppy',
-    'version'       :      	"0.0.0",  # will be replaced below
-    'description'   :       'Physical optics propagation (wavefront diffraction) for optical simulations, particularly of telescopes.',
-    'fullname'      :       'Physical Optics Propagation in PYthon (POPPY)',
-    'author'        :     	"Marshall Perrin",
-    'author_email'  :      	"mperrin@stsci.edu",
-    'url'           :  		"http://www.stsci.edu/~mperrin/software/webbpsf",
-    'download_url'           :  		"http://www.stsci.edu/~mperrin/software/webbpsf/poppy-0.0.0.tar.gz",  # will be replaced below
-    'platforms'     :      	["Linux","Mac OS X", "Win"],
-    'requires'      :       ['pyfits','numpy', 'matplotlib', 'scipy', 'asciitable'],
-    'packages'      :       ['poppy'],
-    'test_suite'    :       'poppy.tests',
-    'classifiers'   :   [
-        "Programming Language :: Python",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering :: Astronomy",
-        "Development Status :: 4 - Beta"
-        ],
-    'long_description': """
-
-Physical Optics Propagation in PYthon (POPPY)
+PACKAGENAME = 'poppy'
+DESCRIPTION = 'Physical optics propagation (wavefront diffraction) for optical simulations, particularly of telescopes.'
+LONG_DESCRIPTION = """Physical Optics Propagation in PYthon (POPPY)
 ------------------------------------------------
 
-    This module implements an object-oriented system for modeling physical optics propagation with diffraction, particularly for multiwavelength simulations of telescopic and coronagraphic imaging. 
-    
-    Right now only far-field diffraction (Fraunhofer regime) between image and pupil planes is supported; Fresnel propagation and intermediate planes are a future goal.
+This module implements an object-oriented system for modeling physical optics propagation with diffraction, particularly for multiwavelength simulations of telescopic and coronagraphic imaging. 
 
-    Developed by Marshall Perrin at STScI, 2010-2012, for use simulating the James Webb Space Telescope. 
+Right now only far-field diffraction (Fraunhofer regime) between image and pupil planes is supported; Fresnel propagation and intermediate planes are a future goal.
 
-    Documentation can be found online at http://www.stsci.edu/jwst/software/webbpsf/
+Developed by Marshall Perrin at STScI, 2010-2012, for use simulating the James Webb Space Telescope. 
 
-
-    """
-#    'entry_points'  :       {'gui_scripts': ['webbpsfgui = webbpsf.gui',]} # should create exe file on Windows?
-#    'data_files'    :     	[ ( pkg+'/data',  ['data/*' ] ),
-#                                ( pkg+'/tests', [ 'tests/*'] ),
- #                           ]
-    }
-
-#============ read in the version number from the code itself, following  ============
-# http://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
-
-# don't actually import the _version.py, for the reasons described on that web page. 
-import re
-VERSIONFILE="poppy/_version.py"
-verstrline = open(VERSIONFILE, "rt").read()
-VSRE = r"__version__ = ['\"]([^'\"]*)['\"]"
-mo = re.search(VSRE, verstrline, re.M)
-if mo:
-    setupargs['version'] = mo.group(1)
-    setupargs['download_url'] = "http://www.stsci.edu/~mperrin/software/webbpsf/poppy-"+mo.group(1)+".tar.gz"
-else:
-    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+Documentation can be found online at http://www.stsci.edu/jwst/software/webbpsf/
+"""
+AUTHOR = 'Marshall Perrin'
+AUTHOR_EMAIL = 'mperrin@stsci.edu'
+LICENSE = 'BSD'
+URL = 'http://www.stsci.edu/~mperrin/software/webbpsf'
 
 
 
-# Now actually call setup
+# VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
+VERSION = '0.3.dev'
 
-setup(**setupargs)
+# Indicates if this version is a release version
+RELEASE = 'dev' not in VERSION
+
+if not RELEASE:
+    VERSION += get_git_devstr(False)
+
+#DOWNLOAD_BASE_URL = 'http://pypi.python.org/packages/source/p/poppy'
+
+# Populate the dict of setup command overrides; this should be done before
+# invoking any other functionality from distutils since it can potentially
+# modify distutils' behavior.
+cmdclassd = register_commands(PACKAGENAME, VERSION, RELEASE)
+
+# Adjust the compiler in case the default on this platform is to use a
+# broken one.
+adjust_compiler(PACKAGENAME)
+
+# Freeze build information in version.py
+generate_version_py(PACKAGENAME, VERSION, RELEASE, get_debug_option())
+
+## 
+## 
+## setupargs = {
+##     'name'          :       PACKAGENAME,
+##     'version'       :      	VERSION,
+##     'fullname'      :       'Physical Optics Propagation in PYthon (POPPY)',
+##     'author'        :     	"Marshall Perrin",
+##     'author_email'  :      	"mperrin@stsci.edu",
+##     'url'           :  		"http://www.stsci.edu/~mperrin/software/webbpsf",
+##     'download_url'           :  		"http://www.stsci.edu/~mperrin/software/webbpsf/poppy-0.0.0.tar.gz",  # will be replaced below
+##     'platforms'     :      	["Linux","Mac OS X", "Win"],
+##     'requires'      :       ['pyfits','numpy', 'matplotlib', 'scipy', 'asciitable'],
+##     'packages'      :       ['poppy'],
+##     'test_suite'    :       'poppy.tests',
+##     'classifiers'   :   [
+##         "Programming Language :: Python",
+##         "License :: OSI Approved :: BSD License",
+##         "Operating System :: OS Independent",
+##         "Intended Audience :: Science/Research",
+##         "Topic :: Scientific/Engineering :: Astronomy",
+##         'Topic :: Scientific/Engineering :: Physics',
+##         "Development Status :: 4 - Beta"
+##         ],
+##     'long_description': """
+## 
+## Physical Optics Propagation in PYthon (POPPY)
+## ------------------------------------------------
+## 
+##     This module implements an object-oriented system for modeling physical optics propagation with diffraction, particularly for multiwavelength simulations of telescopic and coronagraphic imaging. 
+##     
+##     Right now only far-field diffraction (Fraunhofer regime) between image and pupil planes is supported; Fresnel propagation and intermediate planes are a future goal.
+## 
+##     Developed by Marshall Perrin at STScI, 2010-2012, for use simulating the James Webb Space Telescope. 
+## 
+##     Documentation can be found online at http://www.stsci.edu/jwst/software/webbpsf/
+## 
+## 
+##     """
+## #    'entry_points'  :       {'gui_scripts': ['webbpsfgui = webbpsf.gui',]} # should create exe file on Windows?
+## #    'data_files'    :     	[ ( pkg+'/data',  ['data/*' ] ),
+## #                                ( pkg+'/tests', [ 'tests/*'] ),
+##  #                           ]
+##     }
+## # Use the find_packages tool to locate all packages and modules
+packagenames = filter_packages(find_packages())
+
+# Treat everything in scripts except README.rst as a script to be installed
+scripts = [fname for fname in glob.glob(os.path.join('scripts', '*'))
+           if os.path.basename(fname) != 'README.rst']
+
+# Additional C extensions that are not Cython-based should be added here.
+extensions = []
+
+# A dictionary to keep track of all package data to install
+package_data = {PACKAGENAME: ['data/*']}
+
+# A dictionary to keep track of extra packagedir mappings
+package_dirs = {}
+
+# Update extensions, package_data, packagenames and package_dirs from
+# any sub-packages that define their own extension modules and package
+# data.  See the docstring for setup_helpers.update_package_files for
+# more details.
+update_package_files(PACKAGENAME, extensions, package_data, packagenames,
+                     package_dirs)
+
+
+setup(name=PACKAGENAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      packages=packagenames,
+      package_data=package_data,
+      package_dir=package_dirs,
+      ext_modules=extensions,
+      scripts=scripts,
+      requires=['astropy'],
+      install_requires=['astropy'],
+      provides=[PACKAGENAME],
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      license=LICENSE,
+      url=URL,
+      long_description=LONG_DESCRIPTION,
+      cmdclass=cmdclassd,
+      zip_safe=False,
+      use_2to3=False
+      )
