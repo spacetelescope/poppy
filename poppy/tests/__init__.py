b44b9dfee2a2768e4dbcd9ed16cc23acd6cd7fb9
Marshall Perrin <mperrin@stsci.edu>
2014-05-14 16:28:32 -0400

    more progress toward merging in astropy affiliate template git history

--- poppy/tests/__init__.py
+++ poppy/tests/__init__.py
@@ -1,4 +1,4 @@
-#Test code for POPPY - Use the function test() in poppy itself to invoke this. 
-#
-# Test pattern based on http://ssb.stsci.edu/testing/pandokia/docs_new/helper_testfn.html
-from __future__ import division
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+This packages contains affiliated package tests.
+"""
---
+++ poppy_original/tests/__init__.py
@@ -0,0 +1,4 @@
+#Test code for POPPY - Use the function test() in poppy itself to invoke this. 
+#
+# Test pattern based on http://ssb.stsci.edu/testing/pandokia/docs_new/helper_testfn.html
+from __future__ import division
