9bd1a63ab7a95ec70b18ecb1d342fa72f60d171a
Marshall Perrin <mperrin@stsci.edu>
2014-05-17 23:04:08 -0400

    Improve and extend unit tests

---
+++ poppy/tests/test_fwcentroid.py
@@ -0,0 +1,57 @@
+
+import numpy as np
+from ..fwcentroid import test_fwcentroid
+
+# fwcentroid is a standalong package that's just included as a copy in
+# poppy. It has its own test function
+
+def test_fwcentroid_square(n=20,):
+    test_fwcentroid(n=n,verbose=False)
+
+def test_fwcentroid_rectangle(n=20,):
+    test_fwcentroid(n=20,halfwidth=[5,7],verbose=False)
+
+
+
+
+#
+#def gaussian(height, center_x, center_y, width_x, width_y):
+#    """Returns a gaussian function with the given parameters"""
+#    width_x = float(width_x)
+#    width_y = float(width_y)
+#    return lambda x,y: height*np.exp(
+#                -(((center_x-x)/width_x)**2+((center_y-y)/width_y)**2)/2)
+#
+#
+#def makegaussian(size=128, center=(64,64), width=5):
+#    x = np.arange(size)[np.newaxis,:]
+#    y = np.arange(size)[:,np.newaxis]
+#    arr = gaussian(1, center[0], center[1], width, width)(x,y)
+#    return arr
+#
+#
+#def test_fwcentroid(n=20, width=5, halfwidth=5, **kwargs):
+#    """ Test floating window centroid position is accurate to within
+#    5e-3 pixels for a set of randomly positioned Gaussians, based
+#    on the rms position offset over that set
+#    """
+#
+#    #print("Performing {0} tests using Gaussian PSF with width={1:.1f}, centroid halfwidth= {2:.1f}".format(n,width, halfwidth))
+#    
+#    diffx = np.zeros(n)
+#    diffy = np.zeros(n)
+#    size = 100
+#    for i in range(n):
+#        coords = np.random.uniform(halfwidth+1,size-halfwidth-1,(2))
+#        im = makegaussian(size=size, center=coords, width=width) #, **kwargs)
+#        measy, measx = fwcentroid(im, halfwidth=halfwidth, **kwargs)
+#        diffx[i] = coords[0] - measx
+#        diffy[i] = coords[1] - measy
+#
+#    assert np.sqrt(np.mean(diffx**2+diffy**2)) < 5e-3
+#
+#    #print("RMS measured position error, X: {0} pixels".format(diffx.std()) )
+#    #print("RMS measured position error, Y: {0} pixels".format(diffy.std()) )
+#
+
+
--- poppy/tests/test_optics.py
+++ poppy/tests/test_optics.py
@@ -75,3 +75,59 @@ def test_ParityTestAperture():
 
 
 
+def test_IdealRectangularFieldStop():
+    fs = poppy_core.IdealRectangularFieldStop(width=1, height=10)
+    wave = poppy_core.Wavefront(npix=100, pixelscale=0.1, wavelength=1e-6) # 10x10 arcsec square
+
+    wave*= fs
+    assert wave.shape[0] == 100
+    assert wave.intensity.sum() == 1000 # 1/10 of the 1e4 element array
+
+
+def test_IdealBarOcculter():
+    optic= poppy_core.IdealBarOcculter(width=1, angle=0)
+    wave = poppy_core.Wavefront(npix=100, pixelscale=0.1, wavelength=1e-6) # 10x10 arcsec square
+
+    wave*= optic
+    assert wave.shape[0] == 100
+    assert wave.intensity.sum() == 9000 # 9/10 of the 1e4 element array
+
+#def test_rotations_IdealRectangularFieldStop():
+#
+#    # First let's do a rotation of the wavefront itself by 90^0 after an optic
+#
+#    # now try a 90^0 rotation for the field stop at that optic. Assuming perfect system w/ no aberrations when comparing rsults. ? 
+#    fs = poppy_core.IdealRectangularFieldStop(width=1, height=10, ang;le=90)
+#    wave = poppy_core.Wavefront(npix=100, pixelscale=0.1, wavelength=1e-6) # 10x10 arcsec square
+#
+#    wave*= fs
+#    assert wave.shape[0] == 100
+#    assert fs.intensity.sum() == 1000 # 1/10 of the 1e4 element array
+#
+#
+
+def test_RectangleAperture():
+    optic= poppy_core.RectangleAperture(width=5, height=3)
+    wave = poppy_core.Wavefront(npix=100, diam=10.0, wavelength=1e-6) # 10x10 arcsec square
+    wave*= optic
+    assert wave.shape[0] == 100
+    assert wave.intensity.sum() == 1500 # 50*30 pixels of the 1e4 element array
+
+    optic= poppy_core.RectangleAperture(width=2, height=7)
+    wave = poppy_core.Wavefront(npix=100, diam=10.0, wavelength=1e-6) # 10x10 arcsec square
+    wave*= optic
+    assert wave.shape[0] == 100
+    assert wave.intensity.sum() == 1400 # 50*30 pixels of the 1e4 element array
+
+
+
+
+def test_HexagonAperture():
+    # should make hexagon PSF and compare to analytic expression
+    pass
+
+def test_MultiHexagonAperture():
+    # should make multihexagon PSF and compare to analytic expression
+    pass
+
+
