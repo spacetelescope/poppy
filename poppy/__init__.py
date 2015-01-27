491f7d596ba656d0cc5e44feada82d13d7048b13
Joseph Long <jdl@fastmail.fm>
2015-01-26 21:34:28 -0500

    Fix Sphinx not picking up some classes under popppy.*

--- poppy/__init__.py
+++ poppy/__init__.py
@@ -29,7 +29,6 @@ import astropy as _astropy
 if _astropy.version.major + _astropy.version.minor*0.1 < 0.4:
     raise ImportError("astropy >= 0.4 is required for this version of poppy.")
 
-
 from astropy import config as _config
 class Conf(_config.ConfigNamespace):
     """ 
@@ -76,7 +75,6 @@ class Conf(_config.ConfigNamespace):
 
 conf = Conf()
 
-
 from . import poppy_core
 from . import utils
 from . import optics
@@ -85,22 +83,13 @@ from .poppy_core import *
 from .utils import * 
 from .optics import *
 
- #(display_PSF, display_PSF_difference, display_EE, display_profiles, radial_profile,
- #   measure_EE, measure_radial, measure_fwhm, measure_sharpness, measure_centroid, measure_strehl, measure_anisotropy,
- #   specFromSpectralType, rebin_array)
-
 from .instrument import Instrument
 
 # Not yet implemented:
 #from .wfe import ZernikeWFE, PowerSpectralDensityWFE, KolmogorovWFE
 
+if conf.autosave_fftw_wisdom:  # if we might have autosaved, then auto reload as well
+    # the following will just return if FFTW is not present
+    utils.fftw_load_wisdom()
 
-
-if conf.autosave_fftw_wisdom: # if we might have autosaved, then auto reload as well
-   # the following will just return if FFTW is not present
-   utils.fftw_load_wisdom()
-
-
-__all__ = ['conf','Instrument'] +  utils.__all__ + poppy_core.__all__
-
-
+__all__ = ['conf', 'Instrument'] + utils.__all__ + poppy_core.__all__ + optics.__all__
