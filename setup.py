fad800863482ba8787af99bc5d3f2bddc1ab24a7
Marshall Perrin <mperrin@stsci.edu>
2015-05-20 07:05:27 -0700

    matplotlib 1.3 should be ok too

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ setup(name=PACKAGENAME,
           'six>=1.7.3',
           'numpy>=1.8.0',
           'scipy>=0.14.0',
-          'matplotlib>=1.4.2',
+          'matplotlib>=1.3.0',
           'astropy>=1.0.1'
       ],
       provides=[PACKAGENAME],
