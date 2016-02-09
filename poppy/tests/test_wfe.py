0152c0c0b376b9e153415e9770380ac262a248cd
Joseph Long <jlong@stsci.edu>
2016-02-09 18:34:22 -0500

    Clean up test_wfe.py, add tests for get_opd

--- poppy/tests/test_wfe.py
+++ poppy/tests/test_wfe.py
@@ -7,20 +7,22 @@ from .. import optics
 from .. import zernike
 from .. import wfe
 
+NWAVES = 0.5
+WAVELENGTH = 1e-6
+RADIUS = 1.0
+NPIX = 101
+DIAM = 3.0
+
 def test_ZernikeAberration():
     # verify that we can reproduce the same behavior as ThinLens
     # using ZernikeAberration
-    NWAVES = 0.5
-    WAVELENGTH = 1e-6
-    RADIUS = 1.0
-
-    pupil = optics.CircularAperture(radius=1)
+    pupil = optics.CircularAperture(radius=RADIUS)
     lens = optics.ThinLens(nwaves=NWAVES, reference_wavelength=WAVELENGTH, radius=RADIUS)
-    tl_wave = poppy_core.Wavefront(npix=101, diam=3.0, wavelength=WAVELENGTH)  # 10x10 meter square
+    tl_wave = poppy_core.Wavefront(npix=NPIX, diam=DIAM, wavelength=WAVELENGTH)
     tl_wave *= pupil
     tl_wave *= lens
 
-    zern_wave = poppy_core.Wavefront(npix=101, diam=3.0, wavelength=WAVELENGTH)  # 10x10 meter square
+    zern_wave = poppy_core.Wavefront(npix=NPIX, diam=DIAM, wavelength=WAVELENGTH)
     zernike_lens = wfe.ZernikeWFE(
         coefficients=[0, 0, 0, NWAVES * WAVELENGTH / (2 * np.sqrt(3))],
         radius=RADIUS
@@ -32,6 +34,25 @@ def test_ZernikeAberration():
 
     assert stddev < 1e-16, ("ZernikeAberration disagrees with ThinLens! stddev {}".format(stddev))
 
+def test_wavefront_or_meters_decorator():
+    zernike_lens = wfe.ZernikeWFE(
+        coefficients=[0, 0, 0, NWAVES * WAVELENGTH / (2 * np.sqrt(3))],
+        radius=RADIUS
+    )
+    opd_waves_a = zernike_lens.get_opd(WAVELENGTH)
+    opd_waves_b = zernike_lens.get_opd(poppy_core.Wavefront(wavelength=WAVELENGTH))
+
+    stddev = np.std(opd_waves_a - opd_waves_b)
+    assert stddev < 1e-16, "OPD map disagreement based on form of argument to get_opd!"
+
+def test_zernike_get_opd():
+    zernike_optic = wfe.ZernikeWFE(coefficients=[NWAVES * WAVELENGTH,], radius=RADIUS)
+    opd_map = zernike_optic.get_opd(WAVELENGTH, units='meters')
+    assert np.max(opd_map) == NWAVES * WAVELENGTH
+
+    opd_map_waves = zernike_optic.get_opd(WAVELENGTH, units='waves')
+    assert np.max(opd_map_waves) == NWAVES
+
 def test_ParameterizedAberration():
     # verify that we can reproduce the same behavior as ZernikeAberration
     # using ParameterizedAberration
@@ -39,9 +60,9 @@ def test_ParameterizedAberration():
     WAVELENGTH = 1e-6
     RADIUS = 1.0
 
-    pupil = optics.CircularAperture(radius=1)
+    pupil = optics.CircularAperture(radius=RADIUS)
 
-    zern_wave = poppy_core.Wavefront(npix=101, diam=3.0, wavelength=1e-6)  # 10x10 meter square
+    zern_wave = poppy_core.Wavefront(npix=NPIX, diam=DIAM, wavelength=1e-6)
     zernike_wfe = wfe.ZernikeWFE(
         coefficients=[0, 0, 2e-7, NWAVES * WAVELENGTH / (2 * np.sqrt(3)), 0, 3e-8],
         radius=RADIUS
@@ -55,7 +76,7 @@ def test_ParameterizedAberration():
         radius=RADIUS
     )
 
-    pd_wave = poppy_core.Wavefront(npix=101, diam=3.0, wavelength=1e-6) # 10x10 meter square
+    pd_wave = poppy_core.Wavefront(npix=NPIX, diam=3.0, wavelength=1e-6)
     pd_wave *= pupil
     pd_wave *= parameterized_distortion
 
