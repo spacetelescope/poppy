44df8da1c198aff6137f90a342e64820d5025c38
Marshall Perrin <mperrin@stsci.edu>
2016-05-31 09:43:28 -0400

    add optional optic display annotation capability

--- poppy/poppy_core.py
+++ poppy/poppy_core.py
@@ -2333,6 +2333,12 @@ class OpticalElement(object):
         if crosshairs:
             ax.axhline(0, ls=":", color='k')
             ax.axvline(0, ls=":", color='k')
+
+        if hasattr(self,'display_annotate'):
+            self.display_annotate(self)  # atypical calling convention needed empirically
+                                         # since Python doesn't seem to automatically pass
+                                         # self as first argument for functions added at 
+                                         # run time as attributes? 
         return ax
 
     def __str__(self):
