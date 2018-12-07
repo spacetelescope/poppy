Fix sign of rotation for FITOpticalElement. Fixes #275

poppy/poppy_core.py
@@ -2245,7 +2245,7 @@ class FITSOpticalElement(OpticalElement):
     def __init__(self, name="unnamed optic", transmission=None, opd=None, opdunits=None,
                  rotation=None, pixelscale=None, planetype=None,
                  transmission_index=None, opd_index=None,
-                 shift=None, shift_x=None, shift_y=None, 
+                 shift=None, shift_x=None, shift_y=None,
                  flip_x=False, flip_y=False,
                  **kwargs):
 
@@ -2400,11 +2400,11 @@ class FITSOpticalElement(OpticalElement):
             if rotation is not None and len(self.amplitude.shape) == 2:
                 # do rotation with interpolation, but try to clean up some of the artifacts afterwards.
                 # this is imperfect at best, of course...
-                self.amplitude = scipy.ndimage.interpolation.rotate(self.amplitude, rotation,
+                self.amplitude = scipy.ndimage.interpolation.rotate(self.amplitude, -rotation, #negative = CCW
                                                                     reshape=False).clip(min=0, max=1.0)
                 wnoise = np.where((self.amplitude < 1e-3) & (self.amplitude > 0))
                 self.amplitude[wnoise] = 0
-                self.opd = scipy.ndimage.interpolation.rotate(self.opd, rotation, reshape=False)
+                self.opd = scipy.ndimage.interpolation.rotate(self.opd, -rotation, reshape=False) # negative = CCW
                 _log.info("  Rotated optic by %f degrees counter clockwise." % rotation)
                 self._rotation = rotation
 

poppy/tests/test_optics.py
@@ -73,6 +73,9 @@ def test_roundtrip_through_FITS():
 
 def test_shifting_optics( npix=30,  grid_size = 3, display=False):
     """Test shifting (translation) of Analytic and FITS Optical elements.
+    Does shifting work as expected? Is it consistent between the two classes?
+
+    Tests the fix for #247
     """
     import poppy
     pixsize =grid_size/npix
@@ -99,8 +102,8 @@ def test_shifting_optics( npix=30,  grid_size = 3, display=False):
                                        err_msg="Shifting Analytic and FITS versions are not consistent (v1, via shift_x)")
 
     # FITSOpticalElement also lets you specify shifts via fraction of the array. Let's
-    # show that is  consistent.
-
+    # show that is  consistent.  This is older syntax that is discouraged, and may be
+    # deprecated and removed eventually. But while available it should be correct.
     array_frac = shift_size/grid_size
     fits_shifted_v2 = poppy.FITSOpticalElement(transmission=circ_fits, shift=(array_frac, 0))
     np.testing.assert_allclose(fits_shifted.amplitude, fits_shifted_v2.amplitude, atol=1e-9,
@@ -115,6 +118,42 @@ def test_shifting_optics( npix=30,  grid_size = 3, display=False):
     shift_in_pixels = int(shift_size/pixsize)
     assert np.allclose(np.roll(circ_samp[npix//2], shift_in_pixels),
                                circ_shift_samp[npix//2])
+
+
+def test_shift_rotation_consistency(npix=30, grid_size = 1.5, angle=35, display=False):
+    """Test shifting & rotation together for FITS and Analytic optics
+    Do we get consistent behavior from each? Are the signs and
+    order of operations consistent?
+
+    Tests the fix for issue #275.
+    """
+    import poppy
+    if npix < 30:
+        raise ValueError("Need npix>=30 for enough resolution for this test")
+
+    # Create rectangle, rotated
+    rect = poppy.RectangleAperture(shift_x=0.25, rotation=angle)
+    rect_samp = rect.sample(grid_size=grid_size, npix=npix)
+
+    # Create rectangle, turn into FITS, then rotate
+    rect_fits = poppy.RectangleAperture().to_fits(grid_size=grid_size, npix=npix)
+    rect2 = poppy.FITSOpticalElement(transmission=rect_fits, shift_x=0.25, rotation=angle)
+
+    # Compare that they are consistent enough, meaning
+    # no more than 1% pixel difference. That tolerance allows for the
+    # imprecision of rotating low-res binary masks.
+    diff = np.round(rect2.amplitude)-rect_samp
+    assert np.abs(diff).sum() <= 0.01*rect_samp.sum(), "Shift and rotations differ unexpectedly"
+
+    if display:
+        plt.figure()
+        plt.subplot(131)
+        plt.imshow(rect_samp)
+        plt.subplot(132)
+        plt.imshow(np.round(rect2.amplitude))
+        plt.subplot(133)
+        plt.imshow(diff)
+
 
 #------ Analytic Image Plane elements -----
 